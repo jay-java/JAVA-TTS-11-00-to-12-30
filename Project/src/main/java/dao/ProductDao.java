28f8cf7fa63bcb984012837b843aa4da755f7724
jay-java <jayprajapati.tops@gmail.com>
2023-05-06 15:14:56 +0530

    manage product

--- Project/src/main/java/dao/ProductDao.java
+++ Project/src/main/java/dao/ProductDao.java
@@ -2,6 +2,9 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import connection.DBConnection;
 import model.Product;
@@ -20,6 +23,42 @@ public class ProductDao {
 			pst.setString(6, p.getPdescription());
 			pst.executeUpdate();
 			System.out.println("product uploaded");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static List<Product> getProductListBySellerId(int id){
+		List<Product> list  =new ArrayList<Product>();
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql = "select * from product where sid=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			ResultSet rs  =pst.executeQuery();
+			while(rs.next()) {
+				Product p = new Product();
+				p.setPid(rs.getInt("pid"));
+				p.setSid(rs.getInt("sid"));
+				p.setImage(rs.getString("image"));
+				p.setPname(rs.getString("pname"));
+				p.setPprice(rs.getInt("pprice"));
+				p.setPcategory(rs.getString("pcategory"));
+				p.setPdescription(rs.getString("pdesc"));
+				list.add(p);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	public static void deleteProduct(int id) {
+		try {
+			Connection conn = DBConnection.createConnection();
+			String sql = "delete from product where pid=?";
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setInt(1, id);
+			pst.executeUpdate();
+			System.out.println("product deletd");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
